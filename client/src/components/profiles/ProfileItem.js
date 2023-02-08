e2e7c454f583925503cbae9203c68a531e792244
sagar <sagarghori007@gmail.com>
2023-02-08 17:05:36 +0530

    developers set

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -2,34 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const ProfileItem = ({
-  profile: {
-    user: { _id, name, avatar },
-    status,
-    company,
-    location,
-    skills,
-  },
-}) => {
+const ProfileItem = ({ profile }) => {
   return (
     <div className='profile bg-light'>
-      <img src={avatar} alt='' className='round-img' />
+      <img
+        src={
+          Object.keys(profile).length > 0 && profile.user && profile.user.avatar
+        }
+        alt=''
+        className='round-img'
+      />
       <div>
-        <h2>{name}</h2>
+        <h2>
+          {Object.keys(profile).length > 0 && profile.user && profile.user.name}
+        </h2>
         <p>
-          {status} {company && <span> at {company}</span>}
+          {Object.keys(profile).length > 0 && profile.status}{' '}
+          {Object.keys(profile).length > 0 && profile.company && (
+            <span> at {profile.company} </span>
+          )}
         </p>
-        <p className='my-1'>{location && <span>{location}</span>}</p>
-        <Link to={`/profile/${_id}`} className='btn btn-primary'>
+        <p className='my-1'>
+          {' '}
+          {Object.keys(profile).length > 0 && profile.location && (
+            <span> {profile.location}</span>
+          )}
+        </p>
+        <Link
+          to={`/profile/${
+            Object.keys(profile).length > 0 && profile.user && profile.user._id
+          }`}
+          className='btn btn-primary'
+        >
           View Profile
         </Link>
       </div>
       <ul>
-        {skills.slice(0, 4).map((skill, index) => (
-          <li key={index} className='text-primary'>
-            <i className='fas fa-check'></i> {skill}
-          </li>
-        ))}
+        {Object.keys(profile).length > 0 &&
+          profile.skills &&
+          profile.skills.slice(0, 4).map((skill, index) => (
+            <li key={index} className='text-primary'>
+              <i className='fas fa-check'></i>
+              {skill}
+            </li>
+          ))}
       </ul>
     </div>
   );
